dcb804923c3fd0ca4e283f30755f3dc44252e700
Jatin Shah <jatin7@gmail.com>
2019-10-14 17:55:22 -0500

    Updated absolute path

--- data/load_data.sh
+++ data/load_data.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-
 cqlsh -e "CREATE KEYSPACE killrvideo WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-cqlsh -k killrvideo -f /data/schema/schema.cql
-cqlsh -k killrvideo -e "COPY users FROM '/data/csv/users.csv';"
-cqlsh -k killrvideo -e "COPY videos_by_tag FROM '/data/csv/videos_by_tag.csv';"
-cqlsh -k killrvideo -e "COPY user_credentials FROM '/data/csv/user_credentials.csv';"
-cqlsh -k killrvideo -e "COPY tags_by_letter FROM '/data/csv/tags_by_letter.csv';"
-cqlsh -k killrvideo -e "COPY video_playback_stats FROM '/data/csv/video_playback_stats.csv';"
-cqlsh -k killrvideo -e "COPY latest_videos FROM '/data/csv/latest_videos.csv';"
-cqlsh -k killrvideo -e "COPY video_ratings_by_user FROM '/data/csv/video_ratings_by_user.csv';"
-cqlsh -k killrvideo -e "COPY comments_by_user FROM '/data/csv/comments_by_user.csv';"
-cqlsh -k killrvideo -e "COPY videos FROM '/data/csv/videos.csv';"
-cqlsh -k killrvideo -e "COPY comments_by_video FROM '/data/csv/comments_by_video.csv';"
-cqlsh -k killrvideo -e "COPY user_videos FROM '/data/csv/user_videos.csv';"
+cqlsh -k killrvideo -f schema/schema.cql
+cqlsh -k killrvideo -e "COPY users FROM 'csv/users.csv';"
+cqlsh -k killrvideo -e "COPY videos_by_tag FROM 'csv/videos_by_tag.csv';"
+cqlsh -k killrvideo -e "COPY user_credentials FROM 'csv/user_credentials.csv';"
+cqlsh -k killrvideo -e "COPY tags_by_letter FROM 'csv/tags_by_letter.csv';"
+cqlsh -k killrvideo -e "COPY video_playback_stats FROM 'csv/video_playback_stats.csv';"
+cqlsh -k killrvideo -e "COPY latest_videos FROM 'csv/latest_videos.csv';"
+cqlsh -k killrvideo -e "COPY video_ratings_by_user FROM 'csv/video_ratings_by_user.csv';"
+cqlsh -k killrvideo -e "COPY comments_by_user FROM 'csv/comments_by_user.csv';"
+cqlsh -k killrvideo -e "COPY videos FROM 'csv/videos.csv';"
+cqlsh -k killrvideo -e "COPY comments_by_video FROM 'csv/comments_by_video.csv';"
+cqlsh -k killrvideo -e "COPY user_videos FROM 'csv/user_videos.csv';"
